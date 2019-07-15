742117b487bd29399f92f9d2897df1297c8d7c4d
Simon Zhang <muyezhang@gmail.com>
2019-07-16 00:33:47 +0800

    nuxt web 1

---
+++ src/components/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div class="app-header">header</div>
+    <div class="app-content">content</div>
+    <div class="app-footer">footer</div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'indexLayout',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
